4c110f2afc20464bfccecb9d6174681aa0e4368c
김형준 <zelda@wemade.com>
2023-02-17 08:58:44 +0000

    update desc

---
+++ flutter/mastprogs/lib/elements/desc_box.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mastprogs/elements/text_title.dart';
+
+class DescriptionBox extends StatelessWidget {
+  final String title, desc;
+  final double titleSize;
+  const DescriptionBox({
+    Key? key,
+    required this.title,
+    required this.titleSize,
+    required this.desc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextTitle(
+            msg: title,
+            size: titleSize,
+            useDivider: false,
+          ),
+          const SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              // border: Border.all(
+              //   width: 1,
+              //   color: Colors.black,
+              // ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(desc),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- flutter/mastprogs/lib/screens/home_screen.dart
+++ flutter/mastprogs/lib/screens/home_screen.dart
@@ -9,41 +9,80 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        bottom: const TabBar(tabs: [
-          Tab(text: '자기 소개', icon: Icon(Icons.manage_accounts)),
-          Tab(text: '회사 경력', icon: Icon(Icons.business)),
-          Tab(text: '작업 내역', icon: Icon(Icons.work_history)),
-          Tab(text: '개인 프로젝트', icon: Icon(Icons.polyline_sharp)),
-        ]),
+        bottom: TabBar(
+          tabs: [
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.person),
+                  SizedBox(width: 10),
+                  Text('자기 소개')
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.business),
+                  SizedBox(width: 10),
+                  Text('회사 경력')
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.handyman_rounded),
+                  SizedBox(width: 10),
+                  Text('작업 내역')
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.polyline_sharp),
+                  SizedBox(width: 10),
+                  Text('개인 프로젝트')
+                ],
+              ),
+            ),
+          ],
+          unselectedLabelColor: Colors.grey.shade600,
+          indicatorColor: Colors.red,
+          indicatorWeight: 5,
+        ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               children: [
-                const Icon(Icons.person),
+                const Icon(Icons.hail_outlined),
                 const SizedBox(
                   width: 10,
                 ),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        '재직중 - 위메이드',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w100,
-                        ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      '재직중 - 위메이드',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w100,
                       ),
-                      Text(
-                        '이직 관심 있음',
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w100,
-                        ),
+                    ),
+                    Text(
+                      '이직 관심 있음',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w100,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 )
               ],
             ),
